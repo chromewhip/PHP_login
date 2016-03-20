2154052dcd103ba0b339e9d85b7b75d83e6b06be
Connor <connorsteele604@gmail.com>
2016-03-20 02:15:29 -0700

    file system/config checks added into Config class

---
+++ classes/Config.php
@@ -0,0 +1,27 @@
+<?php
+
+class Config {
+    // set as null to check if path exists
+    public static function get($path = null) {
+        //create variable to store config 
+        if($path) {
+            //defines where config comes from
+            //seperates by '/'
+            $config = $GLOBALS['config']; 
+            $path = explode('/', $path);
+
+            foreach($path as $bit) {
+                //if config is set, set config to bit
+                //i.e config = remember
+                //checks against the bit aka cookie_name
+                if(isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+            }
+
+            return $config;
+        }
+        //if path doesn't exsists return false
+        return false;
+    }
+}
--- core/init.php
+++ core/init.php
@@ -2,7 +2,9 @@
 session_start();
 //sets up db and cookies
 $GLOBALS['config'] = array (
-                            'mysql' => array(),
+                            'mysql' => array(
+                                             'host' => '127.0.0.1'
+                                             ),
                             'remember' => array(
                                                 'cookie_name' => 'hash', 
                                                 'cookie_expiry' => 604800
@@ -15,7 +17,7 @@ $GLOBALS['config'] = array (
 //i.e '$db= new DB();'' will have DB required by this 
 //function
 spl_autoload_register(function($class) {
-    require_once 'classes/' . $class . 'php';
+    require_once 'classes/' . $class . '.php';
 });
 
 //can't use spl because it's not a class
