885fb92a15cf47eb7bd24a77a63558d5ac84344d
Connor <connorsteele604@gmail.com>
2016-03-20 03:15:29 -0700

    started making the register class

---
+++ register.php
@@ -0,0 +1,17 @@
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password">
+    </div>
+        <div class="field">
+        <label for="password_again">Enter your password again</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+
+    <input type="submit" value="Register">
+
+</form>
